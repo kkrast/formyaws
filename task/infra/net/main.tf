8cfc51a03fa7cd97f6fbef660331eec3f1a4a898
KalinKrastev <k.krastev@propy.com>
2020-03-21 18:35:18 +0200

    first fixes -2

--- task/infra/net/main.tf
+++ task/infra/net/main.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "my-public1" {
-  vpc_id                  = "${aws_vpc.my-main.id}"
+  vpc_id                  = "aws_vpc.my-main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "${data.aws_availability_zones.available.names[1]}"
   map_public_ip_on_launch = true
@@ -11,7 +11,7 @@ tags {
   }
 }
 resource "aws_subnet" "my-public2" {
-  vpc_id                  = "${aws_vpc.my-main.id}"
+  vpc_id                  = aws_vpc.my-main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${data.aws_availability_zones.available.names[0]}"
   map_public_ip_on_launch = true
@@ -22,7 +22,7 @@ tags {
 }
 
 resource "aws_subnet" "my-private1" {
-  vpc_id                  = "${aws_vpc.my-main.id}"
+  vpc_id                  = "aws_vpc.my-main.id
   cidr_block              = "10.0.3.0/24"
   availability_zone       = "${data.aws_availability_zones.available.names[1]}"
   map_public_ip_on_launch = true
@@ -32,7 +32,7 @@ tags {
   }
 }
 resource "aws_subnet" "my-private2" {
-  vpc_id                  = "${aws_vpc.my-main.id}"
+  vpc_id                  = aws_vpc.my-main.id
   cidr_block              = "10.0.4.0/24"
   availability_zone       = "${data.aws_availability_zones.available.names[0]}"
   map_public_ip_on_launch = true
