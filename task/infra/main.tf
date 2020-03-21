b837b7c441c626ac4e5bdc58d0aaf9e23e0c5e0a
KalinKrastev <k.krastev@propy.com>
2020-03-21 21:41:19 +0200

    move from wrong place for vpc_id def

--- task/infra/main.tf
+++ task/infra/main.tf
@@ -6,14 +6,5 @@ module "security" {
   vpc_id = "${module.network.vpc}"
 }
 
-resource "aws_vpc" "my-main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = false
-  enable_dns_support   = true
-  instance_tenancy     = "default"
-tags = {
-    Site = "my-web-site"
-    Name = "my-vpc"
-  }
-}
+
 
--- task/infra/net/main.tf
+++ task/infra/net/main.tf
@@ -1,3 +1,14 @@
+resource "aws_vpc" "my-main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = false
+  enable_dns_support   = true
+  instance_tenancy     = "default"
+tags = {
+    Site = "my-web-site"
+    Name = "my-vpc"
+  }
+}
+
 data "aws_availability_zones" "available" {}
 
 resource "aws_subnet" "my-public1" {
