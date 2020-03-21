0c16bcff5b61773d856aea7f7bffeb165df3bbea
KalinKrastev <k.krastev@propy.com>
2020-03-22 01:45:42 +0200

    remove second disk from lt

--- task/webapp/app/lt-app-inst.tf
+++ task/webapp/app/lt-app-inst.tf
@@ -6,17 +6,6 @@ data "template_file" "app_server_user_data" {
 resource "aws_launch_template" "lt_app_inst" {
   name = "lt-app-inst"
 
-  block_device_mappings {
-    device_name = "/dev/sda1"
-
-    ebs {
-      delete_on_termination = true
-      encrypted = false
-      volume_size = 50
-      volume_type = "gp2"
-    }
-  }
-
   disable_api_termination = false
 
   image_id = data.aws_ami.amazon-linux-ami.id
