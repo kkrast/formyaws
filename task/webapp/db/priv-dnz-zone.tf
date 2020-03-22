6a7b042491bb826246da693edea063973541b5f9
KalinKrastev <k.krastev@propy.com>
2020-03-22 11:50:26 +0200

    change dns record type to be CNAME

--- task/webapp/db/priv-dnz-zone.tf
+++ task/webapp/db/priv-dnz-zone.tf
@@ -10,7 +10,7 @@ resource "aws_route53_zone" "priv_dns_zone" {
 resource "aws_route53_record" "dns_rec_for_db" {
   zone_id = aws_route53_zone.priv_dns_zone.zone_id
   name    = "db.task.int"
-  type    = "A"
+  type    = "CNAME"
   ttl     = "300"
   records = [trimsuffix(aws_db_instance.my-db.endpoint, ":3306")]
 }
