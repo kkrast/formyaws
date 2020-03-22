839680307d581bd951d7a3dc332b92f31cc86fdb
KalinKrastev <k.krastev@propy.com>
2020-03-22 03:15:00 +0200

    enable dns private hostnames

--- task/infra/net/main.tf
+++ task/infra/net/main.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "my-main" {
   cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = false
+  enable_dns_hostnames = true
   enable_dns_support   = true
   instance_tenancy     = "default"
 tags = {
